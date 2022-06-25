aae3c7f83135b0be107389151a1f0a6dd48a1b55
brightkams <ohakambright@gmail.com>
2022-06-25 11:12:09 +0100

    This is my first commit in Gihub.

---
+++ JavaScripts/JS-Introduction.js
@@ -0,0 +1,26 @@
+
+let heading  = document.querySelector (".firstRow")
+heading.style.backgroundColor = "blue"
+heading.style.color = "white"
+
+let tableName = document.querySelector (".name")
+tableName.innerText = "BRIGHT OHAKAM"
+tableName.style.textAlign = "center"
+tableName.style.backgroundColor = "#fff"
+tableName.style.color = "blue"
+
+
+
+
+let tableHeight = document.querySelector(".myHeight")
+tableHeight.innerText = "158cm"
+tableHeight.style.textAlign = "center"
+tableHeight.style.backgroundColor = "#fff"
+tableHeight.style.color = "blue"
+
+
+let tableCountry = document.querySelector(".myCountry")
+tableCountry.innerText = "NIGERIA"
+tableCountry.style.textAlign = "center"
+tableCountry.style.backgroundColor = "#fff"
+tableCountry.style.color = "blue"
---
+++ README.md
@@ -0,0 +1,2 @@
+The contents of this **Repository** shows a html document linked to a _JavaScript_ code that prints out contents on the screen of a device.
+
